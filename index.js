4228fc38bec9b94bc768d1e57c41e7db0ca8e420
apratimgholap <apratimgholap12@gmail.com>
2023-09-28 11:35:58 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+
+// Allows request from different origin
 const cors = require("cors");
+
+// Allows custom logging format
 const morgan = require("morgan");
 
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 app.use(cors());
 
 morgan.token("req-body", (request) => {
@@ -15,6 +19,42 @@ const generateID = () => Math.floor(Math.random() * 1000);
 app.use(express.json()); // very important
 app.use(morgan(customToken));
 
+// Logs no of phonebook entries and date of request made
+app.get("/info", (request, response) => {
+  const date = new Date().toString();
+  console.log(date);
+  response.send(
+    `<p>Phonebook has info for ${phoneBook.length} people </p> <br\> ${date}`
+  );
+});
+
+// Fetch all persons
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+// Fetch a particular person
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id); // Always rememebr url params are string!s
+  const result = phoneBook.find((entry) => entry.id === id);
+
+  if (result) {
+    return response.json(result);
+  }
+
+  response.status(404).end();
+});
+
+// Deletes entry
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  phoneBook = phoneBook.filter((note) => note.id !== id);
+  console.log(phoneBook);
+  response.status(204).end();
+});
+
+// Create a new person
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
@@ -44,35 +84,8 @@ app.post("/api/persons", (request, response) => {
   response.status(201).end();
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(phoneBook);
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const result = phoneBook.find((entry) => entry.id === id);
-
-  if (result) {
-    return response.json(result);
-  }
-
-  response.status(404).end();
-});
-
-app.get("/info", (request, response) => {
-  const date = new Date().toString();
-  console.log(date);
-  response.send(
-    `<p>Phonebook has info for ${phoneBook.length} people </p> <br\> ${date}`
-  );
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  phoneBook = phoneBook.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
+// We have hardcoded data in server itself
+// We could have stored data in database!
 let phoneBook = [
   {
     id: 1,
@@ -96,6 +109,7 @@ let phoneBook = [
   },
 ];
 
+// process.env.PORT allows the webserver to assign available port
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
